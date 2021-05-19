0ae6874a49dbafcd0273e505409e0c58bd1e9666
Allison Sessé <allisonsesse@gmail.com>
2021-05-19 13:41:52 +0200

    candidates controller + view-show

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,31 @@
+class CandidatesController < ApplicationController
+  before_action :set_position, only: [:new, :create]
+
+  def show
+    @candidate = Candidate.find(params[:id])
+  end
+
+  def new
+    @candidate = Candidate.new
+  end
+
+  def create
+    @candidate = Candidate.new(candidate_params)
+    @candidate.position = @position
+    if @candidate.save
+      redirect_to position_path(@position)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_position
+    @position = Position.find(params[:position_id])
+  end
+
+  def candidate_params
+    params.require(:candidate).permit(:first_name, :email, :linkedin, :angellist, :date)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'positions#index'
-  resources :positions
+  resources :positions do
+    resources :candidates, only: [:new, :create]
+  end
+  resources :candidates, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
